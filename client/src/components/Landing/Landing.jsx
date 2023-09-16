701c3b28f5985f4192bf3666dc9bdda3a83be78c
Lucas Martinelli <martinellilucas8@gmail.com>
2023-09-15 23:03:13 -0300

    carousel

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -1,10 +1,15 @@
 import style from "./Landing.module.css";
 import Carousel from "../Carousel/Carousel";
+import logo from "../../assets/images/logo.png";
 
 const Landing = () => {
   return (
     <div className={style.body}>
-      <h1 className={style.title}>StyleHard</h1>
+      <h1 className={style.title}>
+        <img src={logo} />
+        StyleHard
+        <img src={logo} />
+      </h1>
       <Carousel />
     </div>
   );
