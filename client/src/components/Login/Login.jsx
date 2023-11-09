b15a6b57afa927f44b69cc711bee1925a159fcad
Lucas Martinelli <martinellilucas8@gmail.com>
2023-11-09 14:42:05 -0300

    client logged

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -3,7 +3,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 import style from "./Login.module.css";
 const Login = () => {
   const { user, loginWithRedirect, isAuthenticated } = useAuth0();
-
+  console.log(user?.email_verified);
   return (
     <>
       {!isAuthenticated ? (
