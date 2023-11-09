9eda3f23477d1d410c2df1afeca8e7f09a5c9459
Lucas Martinelli <martinellilucas8@gmail.com>
2023-11-09 14:10:24 -0300

    login button css

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-
+import style from "./Login.module.css";
 const Login = () => {
   const { loginWithRedirect } = useAuth0();
   return (
     <button
+      className={style.button}
       onClick={() => {
         loginWithRedirect();
       }}
